aa15330ad39a214e15e530b1046a0b51f7f2707e
zhangrh <zhangrhweb@163.com>
2017-12-14 16:26:57 +0800

    fixe JSON.stringify

--- controllers/excle.js
+++ controllers/excle.js
@@ -3,8 +3,45 @@ const UpFile = require('../models/qnUpload')
 
 
 
-module.exports = (req, res) => {
 
+function simpleStringify (object){
+  var simpleObject = {};
+  for (var prop in object ){
+      if (!object.hasOwnProperty(prop)){
+          continue;
+      }
+      if (typeof(object[prop]) == 'object'){
+          continue;
+      }
+      if (typeof(object[prop]) == 'function'){
+          continue;
+      }
+      simpleObject[prop] = object[prop];
+  }
+  return JSON.stringify(simpleObject); // returns cleaned up JSON
+};
+
+
+
+function censor(censor) {
+  var i = 0;
+
+  return function(key, value) {
+    if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) 
+      return '[Circular]'; 
+
+    if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
+      return '[Unknown]';
+
+    ++i; // so we know we aren't using the original object anymore
+
+    return value;  
+  }
+}
+
+
+
+module.exports = (req, res) => {
   let fileName = "test"
   let sheetName = "mySheet"
   const jsonData = [
@@ -23,7 +60,8 @@ module.exports = (req, res) => {
       if (err) {
         throw err
       }
-      res.end(JSON.stringify(uploadInfo))
+      res.end(simpleStringify(req))
+      // res.end(JSON.stringify(uploadInfo))
     })
   })
 }
